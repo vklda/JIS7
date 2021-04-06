9c9f4dc58dfee31e78911aa990e39ce4f04fbaad
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2021-04-06 18:21:48 +0300

    Good job
    //********Checked********

--- src/main/java/homework/exeptions/demoDataBase/exceptions/ItemNotFoundException.java
+++ src/main/java/homework/exeptions/demoDataBase/exceptions/ItemNotFoundException.java
@@ -1,5 +1,5 @@
 package homework.exeptions.demoDataBase.exceptions;
-
+//********Checked********
 public class ItemNotFoundException extends RuntimeException{
     public ItemNotFoundException(String message) {
         super(message);
