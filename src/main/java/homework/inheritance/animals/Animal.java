83eecb2b9d8ee2414e1b1529fbc65a4c464b2d7c
vklda <78853752+vklda@users.noreply.github.com>
2021-04-14 14:56:26 +0300

    adds Hw Animals

---
+++ src/main/java/homework/inheritance/animals/Animal.java
@@ -0,0 +1,69 @@
+package homework.inheritance.animals;
+
+import java.util.Objects;
+
+public abstract class Animal {
+    private String name;
+    private int age;
+    private int weight;
+
+    public Animal(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public Animal() {
+    }
+
+    public String eat() {
+        return this.name + " is eating";
+    }
+
+    public String move() {
+        return this.name + " is moving";
+    }
+
+    public abstract String voice();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return age == animal.age && weight == animal.weight && Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                '}';
+    }
+}
