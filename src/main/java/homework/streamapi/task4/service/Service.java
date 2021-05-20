8855734e421b9789e310f5e5a3409da27ae92ebb
vklda <78853752+vklda@users.noreply.github.com>
2021-05-20 14:40:44 +0300

    a little bit refactoring

--- src/main/java/homework/streamapi/task4/service/Service.java
+++ src/main/java/homework/streamapi/task4/service/Service.java
@@ -1,16 +1,17 @@
 package homework.streamapi.task4.service;
 
-import homework.streamapi.task4.App;
 import homework.streamapi.task4.exception.DontFindRequiredPersonException;
 import homework.streamapi.task4.model.Person;
 import homework.streamapi.task4.model.Skill;
 import homework.streamapi.task4.repository.Repository;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static homework.streamapi.task4.App.log;
 import static homework.streamapi.task4.repository.Repository.personList;
 
 public class Service {
@@ -19,12 +20,12 @@ public class Service {
     }
 
     public Person findBestMatchingPerson(String skillName) {
-        Map<Person, Optional<Skill>> collectByRequiredLevel = personList.stream()
+        var collectByRequiredLevel = personList.stream()
                 .collect(Collectors.toMap(Function.identity(), it -> Arrays.stream(it.getSkill())
                         .filter(skill -> skill.getSkillName().equals(skillName))
                         .max((p1, p2) -> p2.getLevel().compareTo(p1.getLevel()))));
 
-        Optional<Person> person = collectByRequiredLevel.entrySet().stream()
+        var person = collectByRequiredLevel.entrySet().stream()
                 .filter(entry -> entry.getValue().isPresent())
                 .max(Comparator.comparing(p -> p.getValue().get().getLevel()))
                 .map(Map.Entry::getKey);
