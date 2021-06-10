4e90bdd776d19e7f058f302457ef7a900985e87e
vklda <78853752+vklda@users.noreply.github.com>
2021-06-10 22:01:21 +0300

    initial commit

--- src/test/java/homework/optional/service/LecturerServiceImplTest.java
+++ src/test/java/homework/optional/service/LecturerServiceImplTest.java
@@ -1,9 +1,13 @@
 package homework.optional.service;
 
+import homework.optional.exceptions.LecturerNotFoundException;
 import homework.optional.exceptions.NullFieldException;
+import homework.optional.model.Lecturer;
 import homework.optional.repository.LecturerRepository;
 import homework.optional.repository.StudentRepository;
 import org.junit.jupiter.api.*;
+
+import java.util.List;
 
 import static homework.optional.repository.LecturerRepository.lecturerList;
 import static homework.optional.repository.StudentRepository.studentList;
@@ -44,23 +48,35 @@ class LecturerServiceImplTest {
     @Test
     void cleanStudents() {
         lecturerService.addStudentsToLecturer(studentList, lecturerList.get(0));
-        Assertions.assertEquals(3,lecturerList.get(0).getStudents().size());
+        Assertions.assertEquals(3, lecturerList.get(0).getStudents().size());
         lecturerService.cleanStudents(lecturerList.get(0));
-        Assertions.assertEquals(0,lecturerList.get(0).getStudents().size());
+        Assertions.assertEquals(0, lecturerList.get(0).getStudents().size());
     }
 
     @Test
     void addStudentsToLecturer() {
         lecturerService.addStudentsToLecturer(studentList, lecturerList.get(0));
-        Assertions.assertEquals(3,lecturerList.get(0).getStudents().size());
+        Assertions.assertEquals(3, lecturerList.get(0).getStudents().size());
     }
 
     @Test
-    void findBySecondName() {
+    void findBySecondNamePositive() {
+        var lecturer1 = lecturerService.findBySecondName("Stankevich");
+        var lecturer2 = lecturerList.get(0);
+        Assertions.assertEquals(lecturer1, lecturer2);
+    }
+
+    @Test
+    void findBySecondNameThrowsException() {
+        Assertions.assertThrows(LecturerNotFoundException.class, () -> {
+            lecturerService.findBySecondName(null);
+        });
     }
 
     @Test
     void findByName() {
+        var lecturers = lecturerService.findByName("Artsemi");
+        Assertions.assertEquals(1, lecturers.size());
     }
 
     @Test
